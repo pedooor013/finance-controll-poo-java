2b54f3c313c4041c6370f6980423c79b89032a5d
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-06-08 19:57:06 -0300

    addd: desenvolvimento de metodos de busca e login dentro do UserDAO, e implementação do PasswordUtils para hash de senhas!

--- src/main/java/com/financecontroll/Main.java
+++ src/main/java/com/financecontroll/Main.java
@@ -1,10 +1,26 @@
 package com.financecontroll;
 
+import com.financecontroll.model.User;
 import com.financecontroll.model.dao.DatabaseConnection;
+import com.financecontroll.model.dao.UserDAO;
 
-class Main{
-    public static void main(String[] args){
+import java.sql.SQLException;
+
+class Main {
+    public static void main(String[] args) throws SQLException {
         DatabaseConnection.getInstance().getConnection();
         System.out.println("Connected to database");
+
+
+        UserDAO userDAO = new UserDAO();
+
+    // Cadastra um usuário
+        User user = new User("Pedro", "pedro@email.com", "123456");
+        userDAO.save(user);
+        System.out.println("User saved!");
+
+    // Verifica login
+        boolean login = userDAO.checkUser("pedro@email.com", "123456");
+        System.out.println("Login: " + login);
     }
 }
--- src/main/java/com/financecontroll/model/dao/UserDAO.java
+++ src/main/java/com/financecontroll/model/dao/UserDAO.java
@@ -1,4 +1,62 @@
 package com.financecontroll.model.dao;
 
+import com.financecontroll.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static com.financecontroll.util.PasswordIUtils.hashPassword;
+
 public class UserDAO {
+    private Connection connection;
+
+    public UserDAO() {
+        this.connection = DatabaseConnection.getInstance().getConnection();
+    }
+
+    public void save(User user) throws SQLException {
+        String sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setString(1, user.getUsername());
+        stmt.setString(2, user.getUserEmail());
+        stmt.setString(3, user.getPassword());
+        stmt.executeUpdate();
+    }
+
+    public User findByEmail(String email) throws SQLException {
+        String sql = "SELECT id, username, email FROM users WHERE email = ?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setString(1, email);
+        ResultSet rs = stmt.executeQuery();
+
+        if(rs.next()){
+            return new User(rs.getInt("id"), rs.getString("username"), rs.getString("email"));
+        }
+        return null;
+    }
+
+    private String findUserPasswordById(int id) throws SQLException {
+        String sql = "SELECT password FROM users WHERE id = ?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+
+        if(rs.next()){
+            return rs.getString("password");
+        }
+        return null;
+    }
+    private boolean checkPassword(String password, String userHashPassword){
+        return userHashPassword.equals(hashPassword(password));
+    }
+
+    public boolean checkUser(String email, String password) throws SQLException {
+        User user = findByEmail(email);
+        if(user != null){
+            return checkPassword(password, findUserPasswordById(user.getId()));
+        }
+        return false;
+    }
+
 }
