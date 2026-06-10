1ec677ef23cb870e223b58ae2f8da0962909b545
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-06-09 21:38:38 -0300

    addd: desenvolvimento da listagem de todas as categories

--- src/main/java/com/financecontroll/model/dao/CategoryDAO.java
+++ src/main/java/com/financecontroll/model/dao/CategoryDAO.java
@@ -1,4 +1,31 @@
 package com.financecontroll.model.dao;
 
+import com.financecontroll.model.Category;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDAO {
+    Connection connection;
+
+    public CategoryDAO() {
+        this.connection = DatabaseConnection.getInstance().getConnection();
+    }
+
+    public List<Category> getAllCategories() throws SQLException {
+        String sql = "SELECT * FROM categories";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+
+        List<Category> categories = new ArrayList<>();
+
+        while(rs.next()){
+            categories.add(new Category(rs.getInt("id"), rs.getString("name")));
+        }
+        return categories;
+    }
 }
