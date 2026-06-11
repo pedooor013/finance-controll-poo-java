98480de3c9573777b17f54be996712a8ca9aab9a
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-06-10 21:43:53 -0300

    addd: desenvolvimento do metodo SAVE, no ExpenseDAO, com tratamento de rollback caso de erro na inserção!

--- src/main/java/com/financecontroll/model/dao/ExpenseDAO.java
+++ src/main/java/com/financecontroll/model/dao/ExpenseDAO.java
@@ -1,4 +1,41 @@
 package com.financecontroll.model.dao;
 
+import com.financecontroll.model.Expense;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ExpenseDAO {
+
+    private Connection connection;
+
+    public ExpenseDAO() {
+        this.connection = DatabaseConnection.getInstance().getConnection();
+    }
+
+    public void save(Expense expense, TransactionDAO transactionDAO) throws SQLException {
+        try {
+            connection.setAutoCommit(false);
+            int transactionId = transactionDAO.save(expense);
+
+            String sql = "INSERT INTO expenses(fk_transaction_id, installments_total, installments_paid, payment_responsible, payment_type, fk_categories_id) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, transactionId);
+            stmt.setInt(2, expense.getInstallmentsTotal());
+            stmt.setInt(3, expense.getInstallmentsPaid());
+            stmt.setString(4, expense.getPaymentResponsible());
+            stmt.setString(5, expense.getPaymentType().toString());
+            stmt.setInt(6, expense.getCategory().getId());
+
+            stmt.executeUpdate();
+            connection.commit();
+
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
 }
